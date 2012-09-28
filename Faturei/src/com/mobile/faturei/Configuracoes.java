ea1c25bf7d2239e1b07b66ea0cf96781d47e565d
francisco.andrade <francisco.andrade@192.168.51.86>
2012-09-28 13:46:37 -0300

    Melhorias: - Inclusa classe de configurações.

---
+++ Faturei/src/com/mobile/faturei/Configuracoes.java
@@ -0,0 +1,85 @@
+package com.mobile.faturei;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class Configuracoes extends Activity {
+	private Button btnVoltar;
+	private DatePicker vencimentoDatePicker;
+	private EditText nomeCartao;
+	private Spinner cartoes;
+    private String array_spinner[];
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes);
+        btnVoltar = (Button) findViewById(R.id.voltarConfiguracoes);
+        vencimentoDatePicker = (DatePicker) findViewById(R.id.vencimentoDatePicker);
+        nomeCartao = (EditText) findViewById(R.id.putNomeCartao);
+        
+        carregaCartoes();
+        
+        btnVoltar.setOnClickListener(new View.OnClickListener() {public void onClick(View arg0) {finish();}});
+    }
+    
+    public void incluirCartao(View view) {
+        switch (view.getId()) {
+        case R.id.okConfig:                    	
+            if (nomeCartao.getText().length() == 0) {
+                Toast.makeText(this, "Favor Inserir o nome do cartão", Toast.LENGTH_LONG).show();
+                return;
+            }
+                        
+            MainActivity.addCartao(nomeCartao.getText().toString(), vencimentoDatePicker.getYear() + "/" + vencimentoDatePicker.getMonth() + "/" + vencimentoDatePicker.getDayOfMonth());
+            
+            Toast.makeText(getApplicationContext(), "INSERIDO COM SUCESSO", Toast.LENGTH_LONG).show();
+            
+            nomeCartao.setText(null);
+            carregaCartoes();
+	        break;
+        }
+      }
+    
+    public void excluirCartao(View view){
+        switch (view.getId()) {
+        case R.id.excluirCartao:
+        	if(cartoes.getCount() < 1){
+        		Toast.makeText(getApplicationContext(), "SEM CARTÕES PARA EXCLUSAO", Toast.LENGTH_LONG).show();
+        		return;
+        	}
+        	
+        	MainActivity.removeCartao(cartoes.getSelectedItem().toString());
+        	
+        	Toast.makeText(getApplicationContext(), "EXCLUIDO COM SUCESSO", Toast.LENGTH_LONG).show();
+        	
+        	carregaCartoes();
+        }
+    }
+    
+    public void carregaCartoes(){        
+        List<String> cartoesLista = MainActivity.getCartoes();
+        int i = 0;
+        array_spinner= new String[cartoesLista.size()];
+        for (String string : cartoesLista) {
+			array_spinner[i] = string;
+        	i++;
+		}
+        
+        cartoes = (Spinner) findViewById(R.id.cartoesLista);
+        
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+		ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, array_spinner);
+        cartoes.setAdapter(adapter);
+        cartoes.setSelection(0);    	
+    }
+}
