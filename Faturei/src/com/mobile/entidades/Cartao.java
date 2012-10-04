3e5319750d0bef271eedb63679e9092e8ea30d7a
francisco.andrade <francisco.andrade@192.168.51.86>
2012-10-04 16:58:40 -0300

    Melhorias: - Criação da classe cartão.

---
+++ Faturei/src/com/mobile/entidades/Cartao.java
@@ -0,0 +1,22 @@
+package com.mobile.entidades;
+
+public class Cartao {
+	private String cartao;
+	private String fechamento;
+	
+	public Cartao() {
+		super();
+	}
+	public String getCartao() {
+		return cartao;
+	}
+	public void setCartao(String cartao) {
+		this.cartao = cartao;
+	}
+	public String getFechamento() {
+		return fechamento;
+	}
+	public void setFechamento(String fechamento) {
+		this.fechamento = fechamento;
+	}
+}
